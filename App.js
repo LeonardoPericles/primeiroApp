610a4aee0add0650c3d858b873dd220fccbdc630
Dev_LeonardoP <104740311+LeonardoPericles@users.noreply.github.com>
2022-12-09 12:24:18 -0300

    Atualização visual

--- App.js
+++ App.js
@@ -7,26 +7,40 @@ import { Input, Button } from 'react-native-elements';
 const Flex = () => {
   return (
     <View style={[styles.container, {
-      flexDirection: "column"
+  
     }]}>
-      <View style={{ flex: 2, alignItems: 'center', paddingTop: 50 }}>
+      <View style={{ flex: 2, alignItems: 'center',}}>
         <Avatar
           size="xlarge"
           rounded
           source={{
-            uri:
-              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.OlnxO753VRgHKDLLDzCKoAHaHw%26pid%3DApi&f=1&ipt=59a1ff5d403b53e3f5467583f0314f680decd0b8e692331248a1493e330a02a7&ipo=images',
+            uri:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.OlnxO753VRgHKDLLDzCKoAHaHw%26pid%3DApi&f=1&ipt=59a1ff5d403b53e3f5467583f0314f680decd0b8e692331248a1493e330a02a7&ipo=images',
           }} />
       </View>
-      <View style={{ flex: 4, margin: 20, }}>
 
-        <Input placeholder='Login' />
-        <Input placeholder='Senha' />
-        <Button title="Logar" />
-        <Button title="Cadastrar" />
+      <View style={{ flex: 5, margin: 20, }}>
+       
+        <Input 
+         label={'Login'}
+         style={styles.paddingInput}
+         placeholder="Digite seu login"/>
+
+        <Input 
+         label={'Senha'}
+         style={styles.paddingInput}
+         placeholder="Digite sua senha" />
+
+        <Button 
+        title="Logar"
+        style={styles.paddingInput} />
+
+        <Button 
+        title={'Criar conta'}
+        style={styles.paddingInput}
+        type={'outline'}
+        titleStyle={{ color: 'red'}} />
+
       </View>
-
-
 
     </View>
   );
@@ -36,9 +50,12 @@ const Flex = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    paddingTop: '100px' ,
+    backgroundColor: 'black',
   },
-  
+  paddingInput: {
+    padding: '10px',
+  },
 });
 
 
